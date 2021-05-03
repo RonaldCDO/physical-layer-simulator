2eca58c72708e610bfa910c44da2ddcaff6e0c58
ronaldcdo <ronaldcesar.eng@gmail.com>
2021-05-03 19:15:58 -0300

    Adding camadaEnlace.hpp and camadaEnlace.cpp

---
+++ source/camadaEnlace.hpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+
+void CamadaEnlaceDadosTransmissora (std::vector<int> quadro);
+
+void CamadaEnlaceDadosTransmissoraEnquadramento (std::vector<int> quadro);
+
+std::vector<int> CamadaEnlaceDadosTransmissoraEnquadramentoContagemDeCaracteres (std::vector<int> quadro);
+
+std::vector<int> CamadaEnlaceDadosTransmissoraEnquadramentoInsercaoDeBytes (std::vector<int> quadro);
+
+void CamadaEnlaceDadosReceptora (std::vector<int> quadro);
+
+void CamadaEnlaceDadosReceptoraEnquadramento (std::vector<int> quadro);
+
+void CamadaEnlaceDadosRepectoraControleDeErro (std::vector<int> quadro);
+
+std::vector<int> CamadaEnlaceDadosReceptoraEnquadramentoContagemDeCaracteres (std::vector<int> quadro);
+
+std::vector<int> CamadaEnlaceDadosReceptoraEnquadramentoInsercaoDeBytes (std::vector<int> quadro);
